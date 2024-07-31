464ba273b238be9c0f24698e720bec260d732cd6
sophieLe256 <ble20304@Sophies-MacBook-Pro.local>
2024-07-31 17:53:22 -0400

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/imalittledino256/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="https://dribbble" className="home__social-icon" target="_blank">
+        <i className="uil uil-dribbble"></i>
+      </a>
+      <a
+        href="https://github.com/sophieLe256/sophieLe256"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+}
+
+export default Social;
